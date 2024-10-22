711e1c3392b392bae7cffa39fbdc66d57a019738
NotNite <hi@notnite.com>
2024-10-22 08:04:31 -0400

    Remove Console.WriteLine calls

--- WebfishingPlus/Mod.cs
+++ WebfishingPlus/Mod.cs
@@ -11,33 +11,18 @@ public class Mod : IMod {
     public static Config Config = null!;
 
     public Mod(IModInterface modInterface) {
-        Console.WriteLine($"WebfishingPlus {Version} initialized");
-
         Config = modInterface.ReadConfig<Config>();
 
         if (Config.ControllerSupport) {
-            Console.WriteLine("WebfishingPlus: Controller support enabled");
             modInterface.RegisterScriptMod(new ControllerInput.PlayerModifier());
             modInterface.RegisterScriptMod(new ControllerInput.Fishing3Modifier());
             modInterface.RegisterScriptMod(new ControllerInput.InputRemapButtonModifier());
         }
 
-        if (Config.MenuTweaks) {
-            Console.WriteLine("WebfishingPlus: Menu tweaks enabled");
-            modInterface.RegisterScriptMod(new MenuTweaks());
-        }
-        if (Config.FixHotbar) {
-            Console.WriteLine("WebfishingPlus: Hotbar fix enabled");
-            modInterface.RegisterScriptMod(new FixHotbar());
-        }
-        if (Config.SortInventory) {
-            Console.WriteLine("WebfishingPlus: Inventory sorting enabled");
-            modInterface.RegisterScriptMod(new InventorySorter());
-        }
-        if (Config.MeteorNotice) {
-            Console.WriteLine("WebfishingPlus: Meteor notice enabled");
-            modInterface.RegisterScriptMod(new MeteorNotice());
-        }
+        if (Config.MenuTweaks) modInterface.RegisterScriptMod(new MenuTweaks());
+        if (Config.FixHotbar) modInterface.RegisterScriptMod(new FixHotbar());
+        if (Config.SortInventory) modInterface.RegisterScriptMod(new InventorySorter());
+        if (Config.MeteorNotice) modInterface.RegisterScriptMod(new MeteorNotice());
     }
 
     public void Dispose() { }
