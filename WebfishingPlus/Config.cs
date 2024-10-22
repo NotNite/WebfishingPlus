1ade6cc3d5a3a1492880c3df0375a4bd5c902924
funcjay <jay@hl2.sh>
2024-10-22 03:45:28 +0200

    Enable controller input by default, disable menu tweaks

--- WebfishingPlus/Config.cs
+++ WebfishingPlus/Config.cs
@@ -3,14 +3,14 @@
 namespace WebfishingPlus;
 
 public class Config {
-    [JsonInclude] public bool ControllerSupport;
+    [JsonInclude] public bool ControllerSupport = true;
     [JsonInclude] public bool ControllerVibration = true;
     [JsonInclude] public double ControllerVibrationStrength = 1.0;
     [JsonInclude] public bool ControllerVibrationLand = true;
     [JsonInclude] public bool ControllerVibrationReel = true;
     [JsonInclude] public bool ControllerVibrationYank = true;
 
-    [JsonInclude] public bool MenuTweaks = true;
+    [JsonInclude] public bool MenuTweaks;
     [JsonInclude] public bool SortInventory;
     [JsonInclude] public bool FixHotbar = true;
     [JsonInclude] public bool MeteorNotice = true;
